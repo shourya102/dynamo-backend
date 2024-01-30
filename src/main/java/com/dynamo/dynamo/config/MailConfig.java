d10a562ad90e62184fd38900ce6b84c6bdb807fb
shourya102 <shourya.sahu456@@gmail.com>
2024-01-30 20:08:47 +0530

    many changes to authorization, added signout

--- src/main/java/com/dynamo/dynamo/config/MailConfig.java
+++ src/main/java/com/dynamo/dynamo/config/MailConfig.java
@@ -3,7 +3,7 @@ package com.dynamo.dynamo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;\
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import java.util.Properties;
 
 @Configuration
